723120ca4f069c262c5cb7ce68c47fbf228df7de
JC <jczhang718@gmail.com>
2020-08-06 16:36:04 +0100

    Added references and a bit of code

---
+++ test.cpp
@@ -0,0 +1,23 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h> 
+#include <iostream>
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "WHEN WE ALL FALL ASLEEP", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    return 0;
+}
