c3d18bc2e163e080fd8f36a9f89f93b458356c55
JC <jczhang718@gmail.com>
2020-08-06 17:07:34 +0100

    PINK

--- test.cpp
+++ test.cpp
@@ -2,6 +2,19 @@
 #include <GLFW/glfw3.h> 
 #include <iostream>
 
+// Function: resize viewport when window resized
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+// Function: Close window when escape pressed
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
 int main()
 {
     glfwInit();
@@ -10,7 +23,8 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "WHEN WE ALL FALL ASLEEP", NULL, NULL);
+    // Create window object
+    GLFWwindow* window = glfwCreateWindow(1920, 1080, "WHEN WE ALL FALL ASLEEP, WHERE DO WE GO", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -19,5 +33,35 @@ int main()
     }
     glfwMakeContextCurrent(window);
 
+    // Initialize  GLAD 
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Set Viewport dimensions 
+    glViewport(0, 0, 800, 600);
+
+    // Dynamic viewport resize
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        // Paint window 
+        glClearColor(1.0f, 84/255.0f, 235/255.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Clean up
+    glfwTerminate();
+
     return 0;
 }
+
